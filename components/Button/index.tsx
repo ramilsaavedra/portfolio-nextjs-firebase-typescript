9b72dba89159048107a4a44a03049b69b760cc15
Ramil Saavedra <ramilsmcl@gmail.com>
2022-07-25 20:42:38 +0800

    button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './Button.module.css';
+
+type ButtonProps = {
+  children?: React.ReactNode;
+  variant?: 'secondary';
+};
+
+const Button: React.FC<
+  ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>
+> = ({ variant, children, className, ...props }) => {
+  return (
+    <button
+      {...props}
+      className={`${className} ${style.button} ${
+        variant === 'secondary' && style['button-secondary']
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
