7fa77d2e22ba2a2960b7cee5d22819d4338291d4
Ramil Saavedra <ramilsmcl@gmail.com>
2022-07-26 12:11:07 +0800

    mock data for projects

---
+++ mock_data/projects.ts
@@ -0,0 +1,49 @@
+const projects = [
+  {
+    title: 'Web Blocker',
+    techs: ['React', 'Typescript', 'Webpack', 'Manifest V3', 'Figma'],
+    description:
+      'A free chrome extension built with React and Typescript with Webpack configuration. It lets the user block distracting website to keep the user focus and improve their productivity.',
+    imageSrc: '/static/images/projects/webblocker.png',
+    homeUrl: 'https://www.webblockerextension.com/',
+    homeText: 'Visit website',
+    codeUrl: 'https://github.com/ramilsaavedra/webblocker-chrome-v2',
+    codeText: 'View source code',
+  },
+  {
+    title: 'Fans.ph',
+    techs: ['Laravel', 'Blade', 'Adobe Illustrator'],
+    description:
+      'A website created so fans can book personalized shout-outs from their favorite celebrities',
+    imageSrc: '/static/images/projects/fansph.png',
+    homeUrl: 'https://www.fans.ph/',
+    homeText: 'Visit website',
+    codeUrl: null,
+    codeText: null,
+  },
+  {
+    title: 'Minimart.ph',
+    techs: ['Laravel', 'PHP', 'Javascript', 'MySQL'],
+    description:
+      'An e-commerce website that was built during the pandemic to help customers to get their daily needs right without leaving their homes',
+    imageSrc: '/static/images/projects/minimartph.png',
+    homeUrl: 'https://www.minimart.ph/',
+    homeText: 'Visit website',
+    codeUrl: 'https://github.com/ramilsaavedra/demo-mm-public',
+    codeText: 'View source code',
+  },
+
+  {
+    title: 'Flowerstore.ph',
+    techs: ['Laravel', 'PHP', 'Javascript', 'MySQL'],
+    description:
+      'An e-commerce flowershop started from the Philippines and slowly expanding to other parts of Asia (Vietnam and Thailand)',
+    imageSrc: '/static/images/projects/flowerstoreph.png',
+    homeUrl: 'https://flowerstore.ph/en',
+    homeText: 'Visit website',
+    codeUrl: 'https://github.com/ramilsaavedra/demo-fs-public',
+    codeText: 'View source code',
+  },
+];
+
+export { projects };
